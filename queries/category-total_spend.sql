9b5de43b139ba4b5e16295363ab120ddd1551bb2
MMargarito <manuel.margarito94@gmail.com>
2026-07-08 21:15:41 -0400

    Added customer_orders-running-total.sql as a query

--- queries/category-total_spend.sql
+++ queries/category-total_spend.sql
@@ -1,6 +1,1 @@
-SELECT p.category, SUM(o.amount) AS total_revenue
-FROM products as p
-LEFT JOIN orders as o
-ON p.product_id = o.product_id
-GROUP BY p.category
-ORDER BY total_revenue DESC;
+SELECT * FROM orders o;
